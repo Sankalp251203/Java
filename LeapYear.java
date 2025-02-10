c6edf1153f1637eed6359fea10f99d07b12aa735
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-02-10 14:34:52 +0530

    Update and rename Q3.java to LeapYear.java

--- LeapYear.java
+++ LeapYear.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q3 {
+public class LeapYear {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Enter a year: ");
