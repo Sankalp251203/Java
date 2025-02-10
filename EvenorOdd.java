8f4bfee916bd0d4a34270393948f919ca5de69d6
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-02-10 14:34:24 +0530

    Update and rename Q2.java to EvenorOdd.java

--- EvenorOdd.java
+++ EvenorOdd.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q2 {
+public class EvenorOdd {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Enter a number: ");
