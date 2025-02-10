5ce7fb9001c6db959190d535d085018a79104b28
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-02-10 14:36:08 +0530

    Update and rename Q5.java to LargestElement.java

--- LargestElement.java
+++ LargestElement.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q5 {
+public class LargestElement {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Enter the number of elements in the array: ");
