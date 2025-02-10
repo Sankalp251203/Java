d4fdeb6fbd05bde43016ca7564c9b5be53ce51c3
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-02-10 14:38:38 +0530

    Update and rename Q8.java to Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q8 {
+public class Factorial {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Enter a number to find its factorial: ");
