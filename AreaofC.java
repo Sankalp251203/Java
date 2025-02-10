564971ae87c0ba34e413fb0af97a2ab2059d1407
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-02-10 14:39:39 +0530

    Update and rename Q9.java to AreaofC.java

--- AreaofC.java
+++ AreaofC.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Q9 {
+public class AreaofC {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Enter Radius: ");
